32956737b6514f67a623fa58ec17fecb291b65d3
Pravinkevat <pravinkevat199@gmail.com>
2024-10-27 05:29:38 +0530

    commit and push

---
+++ src/Automationtesting9x/lab004simplecalculator.java
@@ -0,0 +1,17 @@
+package Automationtesting9x;
+
+public class lab004simplecalculator {
+    public static void main(String[] args) {
+        System.out.println("this is program for simple calculator");
+        int a=20; int b=10;
+        int c,d,e,f;
+        c=a+b;
+        d=a-b;
+        e=a*b;
+        f=a/b;
+        System.out.println("sum of "+a+" and "+b+" is "+c);
+        System.out.println("substraction of "+a+" and "+b+"is "+d);
+        System.out.println("multification of "+a+" and "+b+" is "+e);
+        System.out.println("division of "+a+" and "+b+" is "+f);
+    }
+}
