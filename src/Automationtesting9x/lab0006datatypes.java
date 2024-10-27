fb5788288d473246e036e49af15ab8b593ac70fe
Pravinkevat <pravinkevat199@gmail.com>
2024-10-27 15:11:42 +0530

    commit and pushtest

---
+++ src/Automationtesting9x/lab0006datatypes.java
@@ -0,0 +1,23 @@
+package Automationtesting9x;
+
+import java.time.LocalTime;
+
+public class lab0006datatypes {
+    public static void main(String[] args) {
+        System.out.println("this program is for different data types");
+//        for boolean
+        boolean is_iam_maried=true;
+        boolean $=false;
+
+        System.out.println(is_iam_maried);
+        System.out.println($);
+
+//        for byte
+        byte a=-128;
+        System.out.println(a);
+//        program for char
+        char s='A';
+        char p= 'P';
+        System.out.println(s+" and "+p);
+    }
+}
