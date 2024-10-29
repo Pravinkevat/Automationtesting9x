15657b06718802aaccd017df0f7dc27f9309b64b
Pravinkevat <pravinkevat199@gmail.com>
2024-10-29 05:36:47 +0530

    commit and pushtestr

---
+++ src/Automationtesting9x/lab007printfprintlnprint.java
@@ -0,0 +1,19 @@
+package Automationtesting9x;
+
+public class lab007printfprintlnprint {
+    public static void main(String[] args) {
+        System.out.println("this program is for difference between println , printf and print ");
+//        program using println
+        System.out.println("this is the example for println");
+        System.out.println("this also example of println");
+
+//  program for print
+        System.out.print("this is the example of print");
+        System.out.print("this also example of print");
+//  program using printf (concitanation)
+        System.out.println();
+        int age=80;
+        System.out.printf("age is %d",age);
+
+    }
+}
---
+++ src/Automationtesting9x/lab08tableusingprintf.java
@@ -0,0 +1,30 @@
+package Automationtesting9x;
+
+public class lab08tableusingprintf {
+    public static void main(String[] args) {
+        System.out.println("this program is for print a table using printf or using concitanation process");
+        int table =45;
+        System.out.printf("%d*%d=%d",table,1, table);
+        System.out.println();
+        System.out.printf("%d*%d=%d",table,2,table*2);
+        System.out.println();
+        System.out.printf("%d*%d=%d",table,3,table*3);
+        System.out.println();
+        System.out.printf("%d*%d=%d",table,4,table*4);
+        System.out.println();
+        System.out.printf("%d*%d=%d",table,5,table*5);
+        System.out.println();
+        System.out.printf("%d*%d=%d",table,6,table*6);
+        System.out.println();
+        System.out.printf("%d*%d=%d",table,7,table*7);
+        System.out.println();
+        System.out.printf("%d*%d=%d",table,8,table*8);
+        System.out.println();
+        System.out.printf("%d*%d=%d",table,9,table*9);
+        System.out.println();
+        System.out.printf("%d*%d=%d",table,10,table*10);
+        System.out.println();
+
+
+    }
+}
